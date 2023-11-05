f5444bbd92956068b0cb42ebcff30b876864f55f
hobiJeong <jjb26433@naver.com>
2023-11-05 10:00:00 +0900

    modify

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
