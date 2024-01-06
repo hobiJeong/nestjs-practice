f343beae559a989a7479a02e62089c637a8c36b9
hobiJeong <jjb26433@naver.com>
2024-01-07 01:34:50 +0900

    redirect test

---
+++ src/users/users.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Res,
+  Redirect,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @Redirect('https://docs.nestjs.com', 302)
+  async findAll() {
+    return { url: 'https//docs.nestjs.com/controllers' };
+    // return this.usersService.findOne(1);
+  }
+
+  @Get(':id')
+  @Redirect('https://docs.nestjs.com', 302)
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
