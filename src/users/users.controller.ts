85556909c7af8071ce502d9687d8a30d80cedf25
hobiJeong <jjb26433@naver.com>
2024-01-12 10:00:00 +0900

    prettier

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
