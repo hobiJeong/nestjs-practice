f1636b84c5b6b359b196ca58f7a5c88de7e17a5d
hobiJeong <jjb26433@naver.com>
2024-01-22 14:46:05 +0900

    feat: class decorator study

---
+++ src/users/decorators/test.decorator.ts
@@ -0,0 +1,7 @@
+export function reportableClassDecorator<
+  T extends { new (...args: any[]): {} },
+>(constructor: T) {
+  return class extends constructor {
+    reportingURL = 'http://www.example.com';
+  };
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,10 +13,20 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Request } from 'express';
+import { reportableClassDecorator } from './decorators/test.decorator';
 
+@reportableClassDecorator
 @Controller({ host: 'localhost' })
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  type = 'report';
+  title: string;
+  constructor(
+    private readonly usersService: UsersService,
+    // t: string,
+    // private readonly t: string,
+  ) {
+    // this.t = t;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -26,6 +36,9 @@ export class UsersController {
   @Get('users')
   async findAll(@Req() req: Request) {
     console.log(req.hostname);
+    const Controller = new UsersController(this.usersService);
+    Controller.title = 'abc';
+    console.log(Controller);
     return 'abc';
   }
 
