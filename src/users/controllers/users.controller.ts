54125ae76433706c217a04d848f05db9df4a0d88
hobiJeong <jjb26433@naver.com>
2024-01-28 19:29:44 +0900

    delete test code

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -10,19 +10,35 @@ import {
   Header,
   Query,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from '../services/users.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { UserLoginDto } from '../dto/user-login-dto';
+import { VerifyEmailDto } from '../dto/verify-email-dto';
+import { UserInfo } from '../interface/user-info.interface';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto): Promise<void> {
     console.log(createUserDto);
-    return createUserDto;
+  }
+
+  @Post('email-verify')
+  verifyEmail(@Query() verifyEmailDto: VerifyEmailDto): Promise<string> {
+    console.log(verifyEmailDto);
+
+    return;
+  }
+
+  @Post('login')
+  login(@Body() userLoginDto: UserLoginDto): Promise<string> {
+    console.log(userLoginDto);
+
+    return;
   }
 
   @Header('NestJS-Book-Study', 'study')
@@ -32,8 +48,9 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 
   @Patch(':id')
