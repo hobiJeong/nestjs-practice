9dfefbadbc3d00b1371e1ffc7b261f7b9a161c62
hobiJeong <jjb26433@naver.com>
2024-02-06 15:44:44 +0900

    npm i joi

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CoreService } from './core.service';
 import { CommonModule } from 'src/common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'stage'
+            ? '.stage.env'
+            : '.local.env',
+    }),
+  ],
   providers: [CoreService],
   exports: [CommonModule],
 })
