c5809dd976945b27a4070b4cfbe2e032d2faf9c3
hobiJeong <jjb26433@naver.com>
2024-02-17 15:48:39 +0900

    refactor: modify type

--- src/apis/auth/jwt/jwt-refresh-token.guard.ts
+++ src/apis/auth/jwt/jwt-refresh-token.guard.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshTokenGuard extends AuthGuard('jwt') {
+export class JwtRefreshTokenGuard extends AuthGuard('jwt-refresh') {
   handleRequest(err, user) {
     if (err || !user) {
       throw new UnauthorizedException('유효하지 않은 토큰입니다.');
