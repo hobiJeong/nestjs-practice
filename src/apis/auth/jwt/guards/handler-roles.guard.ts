1431f8296af626cf213dfaa9c9652808f1743447
hobiJeong <jjb26433@naver.com>
2024-02-19 17:29:12 +0900

    delete: delete unused imports

--- src/apis/auth/jwt/guards/handler-roles.guard.ts
+++ src/apis/auth/jwt/guards/handler-roles.guard.ts
@@ -1,9 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
 import { UserRole } from 'src/apis/users/constants/user-role.enum';
 import { UsersService } from 'src/apis/users/services/users.service';
-import { Payload } from '../../types/auth.type';
 import { USER_ROLE_TOKEN } from 'src/apis/users/constants/user-role.token';
 
 @Injectable()
