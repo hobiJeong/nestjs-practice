15147793a76d382bb15aa54ea7180d65916f38b4
hobiJeong <jjb26433@naver.com>
2024-03-15 02:36:46 +0900

    feat: create errorinterceptor

---
+++ src/interceptors/errors.interceptor.ts
@@ -0,0 +1,16 @@
+import {
+  BadGatewayException,
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Response } from 'src/interceptors/transform.interceptor';
+
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(catchError((err) => throwError(() => new BadGatewayException())));
+  }
+}
