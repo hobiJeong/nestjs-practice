8ae7317278757cebf9369faa305d1f223a5deccf
hobiJeong <jjb26433@naver.com>
2024-03-15 02:57:27 +0900

    feat: create timeout, interval handler

--- src/batch/services/task.service.ts
+++ src/batch/services/task.service.ts
@@ -1,12 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
 
-  @Cron('* * * * * *', { name: 'cronTask' })
+  @Cron(CronExpression.MONDAY_TO_FRIDAY_AT_1PM, { name: 'cronTask' })
   handleCron() {
     this.logger.log('Task Called');
+  }
+
+  @Interval('intervalTask', 3000)
+  handleInterval() {
+    this.logger.log('Task Called by interval');
+  }
+
+  @Timeout('timeoutTask', 5000)
+  handleTimeout() {
+    this.logger.log('Task Called by timeout');
   }
 }
