d548d17a2a9228bdc0487869dfab8d206870ba5e
hobiJeong <jjb26433@naver.com>
2024-03-15 02:36:22 +0900

    feat: new import

--- src/apis/users/controllers/users.controller.ts
+++ src/apis/users/controllers/users.controller.ts
@@ -16,6 +16,7 @@ import {
   Logger,
   BadRequestException,
   SetMetadata,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
