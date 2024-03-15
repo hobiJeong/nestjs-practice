ae8527fb2bf8bff519e7f7457b7fa41346cc13bb
hobiJeong <jjb26433@naver.com>
2024-03-15 20:07:38 +0900

    refactor: modify folder route

--- src/batch/batch.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaskService } from './services/task.service';
-import { ScheduleModule } from '@nestjs/schedule';
-
-@Module({
-  imports: [ScheduleModule.forRoot()],
-  providers: [TaskService],
-})
-export class BatchModule {}
