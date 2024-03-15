dec81a840c3bb000c38302a2e4be0575c14bdaa5
hobiJeong <jjb26433@naver.com>
2024-03-15 20:03:50 +0900

    refactor: push BatchModule in app module's imports array

--- src/apis/api.module.ts
+++ src/apis/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/apis/users/users.module';
 import { ApiController } from './api.controller';
 import { AuthModule } from './auth/auth.module';
+import { BatchModule } from 'src/apis/batch/batch.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, BatchModule],
   controllers: [ApiController],
 })
 export class ApiModule {}
