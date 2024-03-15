65dc20147d2437693b7e3348f7954b10910117c1
hobiJeong <jjb26433@naver.com>
2024-03-15 20:02:47 +0900

    delete: delete bache module at app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { ApiModule } from './apis/api.module';
 import { LoggerModule } from 'src/middlewares/logger.module';
-import { BatchModule } from './batch/batch.module';
+import { BatchModule } from './apis/batch/batch.module';
 
 @Module({
-  imports: [CoreModule, ApiModule, LoggerModule, BatchModule],
+  imports: [CoreModule, ApiModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
