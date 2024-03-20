553019da1545a1737d3ca350c67f519381cbeb72
hobiJeong <jjb26433@naver.com>
2024-03-20 20:41:29 +0900

    feat: injection dog health indicator

--- src/apis/health/health.module.ts
+++ src/apis/health/health.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './controllers/health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { DogHealthIndicator } from 'src/apis/health/indicators/dog-health.indicator';
 
 @Module({
   imports: [TerminusModule, HttpModule],
   controllers: [HealthController],
+  providers: [DogHealthIndicator],
 })
 export class HealthModule {}
