9695dedebfab7a08dca4dfdda46cc8c93d9ee229
hobiJeong <jjb26433@naver.com>
2024-03-20 20:41:51 +0900

    feat: create health check api

--- src/apis/health/controllers/health.controller.ts
+++ src/apis/health/controllers/health.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { DogHealthIndicator } from 'src/apis/health/indicators/dog-health.indicator';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly httpHealthIndicator: HttpHealthIndicator,
+    private readonly db: TypeOrmHealthIndicator,
+    private readonly dogHealthIndicator: DogHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.healthCheckService.check([
+      () =>
+        this.httpHealthIndicator.pingCheck(
+          'nestjs-docs',
+          'https://docs.nestjs.com',
+        ),
+      () => this.db.pingCheck('database'),
+      () => this.dogHealthIndicator.isHealthy('dog'),
+    ]);
+  }
+}
