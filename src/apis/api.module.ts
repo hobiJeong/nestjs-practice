62755787d4b4d9950612f7d8e261d6cd84652b00
hobiJeong <jjb26433@naver.com>
2024-03-20 15:52:16 +0900

    feat: configure health module

--- src/apis/api.module.ts
+++ src/apis/api.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from 'src/apis/users/users.module';
 import { ApiController } from './api.controller';
 import { AuthModule } from './auth/auth.module';
 import { BatchModule } from 'src/apis/batch/batch.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, BatchModule],
+  imports: [UsersModule, AuthModule, BatchModule, HealthModule],
   controllers: [ApiController],
 })
 export class ApiModule {}
