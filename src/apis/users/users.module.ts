d8118d406c37338ba113982e2cd7f1b9bd8e4538
hobiJeong <jjb26433@naver.com>
2024-03-21 21:29:25 +0900

    feat: 핸들러 프로바이더로 등록

--- src/apis/users/users.module.ts
+++ src/apis/users/users.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmExModule } from 'src/core/type-orm/type-orm-ex.module';
 import { UserRepository } from './repository/user.repository';
 import { LoggerModule } from 'src/middlewares/logger.module';
 import { CqrsModule } from '@nestjs/cqrs';
+import { CreateUserHandler } from 'src/apis/users/handlers/create-user.handler';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { CqrsModule } from '@nestjs/cqrs';
     CqrsModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService, Logger],
+  providers: [UsersService, Logger, CreateUserHandler],
   exports: [UsersService],
 })
 export class UsersModule {}
