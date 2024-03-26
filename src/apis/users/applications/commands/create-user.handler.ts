c1a65295110cded586f0c2ec8a58a7eea3480598
hobiJeong <jjb26433@naver.com>
2024-03-26 15:48:59 +0900

    delete: delete useless code

--- src/apis/users/applications/commands/create-user.handler.ts
+++ src/apis/users/applications/commands/create-user.handler.ts
@@ -1,5 +1,5 @@
 import { ConflictException, Inject, Injectable } from '@nestjs/common';
-import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UUID } from 'crypto';
 import { CreateUserCommand } from 'src/apis/users/applications/commands/create-user.command';
 import * as uuid from 'uuid';
@@ -8,9 +8,6 @@ import { UserRole } from 'src/apis/users/constants/user-role.enum';
 import { UserStatus } from 'src/apis/users/constants/user-status.enum';
 import { UserRepository } from 'src/apis/users/infra/db/repository/user.repository';
 import { UserFactory } from 'src/apis/users/domain/user.factory';
-import { IEmailService } from 'src/apis/users/applications/adapter/iemail.service';
-import { EXTERNAL_EMAIL_SERVICE_TOKEN } from 'src/apis/email/constants/external-email-service.token';
-import { EmailService } from 'src/apis/users/infra/adapter/email.service';
 
 @Injectable()
 @CommandHandler(CreateUserCommand)
@@ -19,16 +16,11 @@ export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
     @Inject(UserRepository)
     private readonly userRepository: IUserRepository,
     private readonly userFactory: UserFactory,
-    @Inject(EmailService)
-    private readonly emailService: IEmailService,
-    private readonly eventBus: EventBus,
   ) {}
   async execute(command: CreateUserCommand): Promise<any> {
     const { name, email, password } = command;
 
     const isExistUser = await this.userRepository.findByEmail(email);
-
-    console.log(isExistUser);
 
     if (isExistUser) {
       throw new ConflictException('An email that already exists.');
